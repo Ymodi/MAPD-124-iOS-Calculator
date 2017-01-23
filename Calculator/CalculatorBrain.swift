1dfb858f7033716240ed4a0746a82d89aded1209
Yesha Modi <yesha@Yeshas-MacBook-Pro.local>
2017-01-23 00:31:05 -0500

    Backup before final changes

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,22 @@
+
+  //CalculatorBrain.swift
+  //Calculator
+
+  //Created by Yesha Modi on 2017-01-19.
+  //Copyright © 2017 Yesha Modi-300895482. All rights reserved.
+
+
+import Foundation
+
+class CalculatorBrain
+{
+    func setOperand(operand: Double) {  }
+    
+    func performOperation(symbol: String) { }
+    
+    var result: Double{
+        get{
+            return 0.0
+        }
+    }
+}
